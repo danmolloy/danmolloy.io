db1fdeafe50f00987c7a89e6095000e694010f96
Daniel Molloy <danielmolloy_6@icloud.com>
2023-07-22 14:19:26 +0100

    type error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Layout from '../components/layout'
-import Home from '../components/home'
 import Hero from '../components/hero'
 import ContactSection from '../components/contactSection'
 import AboutSection from '../components/about'
