ede87577404385d05493e43c0312d656344a1e79
Daniel Molloy <danielmolloy_6@icloud.com>
2023-08-18 13:56:36 +0100

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { Inconsolata, Raleway, Poiret_One } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const raleway = Raleway({subsets: ['latin'], variable: "--font-raleway"})
 //const poiretOne = Poiret_One({weight: '400', subsets: ["latin"], variable:"--font-poiret"})
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${raleway.variable} ${inconsolata.variable}`}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   )
 }
