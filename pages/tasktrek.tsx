b5864921f5b1cc89585c6dcaf9ee378e04c542d2
Daniel Molloy <danielmolloy_6@icloud.com>
2023-09-01 18:23:27 +0100

    tasktrek privacy policy

---
+++ pages/tasktrek.tsx
@@ -0,0 +1,71 @@
+export default function TaskTrek() {
+  return (
+    <div className="p-4">
+      <h1>
+      Privacy Policy
+
+      </h1>
+
+<p>
+
+Effective Date: 1 September 2023
+</p>
+
+<p>
+Thank you for using TaskTrek ("the App"). This Privacy Policy is meant to help you understand how we collect, use, disclose, and safeguard your personal information, even though the App does not collect or share any personal data.
+
+</p>
+<h2 className="text-2xl">
+1. Information We Do Not Collect
+</h2>
+<p>
+We want you to know that TaskTrek does not collect any personal information or data from you. This means:
+</p>
+<ul className="text-xl">
+<li>We do not collect your name, email address, or any other contact information.</li>
+<li>We do not collect location information.</li>
+<li>We do not collect device information.</li>
+<li>We do not collect usage data.</li>
+<li>We do not collect any information that you enter or provide while using the App.</li>
+
+</ul>
+<h2  className="text-2xl">
+2. User Consent
+</h2>
+<p>
+By downloading and using TaskTrek, you agree that:
+</p>
+<ul className="text-xl">
+  <li>
+The App does not collect or share your personal information.
+
+  </li>
+  <li>
+
+We do not use cookies, tracking, or analytics to collect data.
+  </li>
+  <li>
+
+We do not request or access any device permissions that could potentially collect data.
+  </li>
+  <li>
+
+The App is designed with your privacy in mind.
+  </li>
+  
+</ul>
+<h2  className="text-2xl">
+3. Changes to this Privacy Policy
+</h2>
+<p>
+
+TaskTrek may update this Privacy Policy from time to time to reflect changes to our practices or for other operational, legal, or regulatory reasons. Any such changes will be posted here with the updated effective date.
+</p>
+<h2  className="text-2xl">
+4. Contact Us
+</h2>
+<p>
+If you have any questions or concerns about our privacy practices or this Privacy Policy, please contact us at danielmolloy_6@icloud.com.</p>
+    </div>
+  )
+}
